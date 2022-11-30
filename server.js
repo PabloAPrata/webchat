ac7837ba7cdf7417e8a16419acad9b2aa84d62c3
Pablo Prata <pabloalmeidaprata@hotmail.com>
2022-11-30 15:55:59 -0300

    Routes adicionado

--- server.js
+++ server.js
@@ -3,5 +3,39 @@ const http = require("http");
 const socketio = require("socket.io");
 const bodyParser = require("body-parser");
 
+const routes = require("./utils/routes");
+const config = require("./utils/routes");
 
-const routes
+class Server {
+  constructor() {
+    this.port = process.env.PORT || 80;
+    this.host = "localhost";
+
+    this.app = express();
+    this.http = http.Server(this.app);
+    this.socket = socket = socketio(this.http);
+  }
+
+  appConfig() {
+    this.app.use(bodyParser.json());
+    new config(this.app, express);
+  }
+
+  // Iniciando o app Routes
+  includeRoutes() {
+    new routes(this.app, this.socket).routesConfig();
+  }
+
+  // Incluindo o fim do app routes
+  appExecute() {
+    this.appConfig();
+    this.includeRoutes();
+
+    this.http.listen(this.port, this.host, () => {
+      console.log(`Listening on http://${this.port}`);
+    });
+  }
+}
+
+const app = new Server();
+app.appExecute();
---
+++ utils/routes.js
@@ -0,0 +1,56 @@
+class Routes {
+  constructor(app, socket) {
+    this.app = app;
+    this.io = socket;
+
+    //Array para armazenar a lista de usuários junto com o socket.io respectivo
+    this.users = [];
+  }
+
+  appRoutes() {
+    this.app.get("/", (req, res) => {
+      response.render("index");
+    });
+  }
+
+  socketEvents() {
+    this.io.on("connection", (socket) => {
+      socket.on("username", (username) => {
+        console.log("Um novo usuário" + username);
+        this.users.push({
+          id: socket.id,
+          username: username,
+        });
+
+        let len = this.users.length;
+        len--;
+        this.io.emit("userList", this.users, this.users[len].id);
+      });
+
+      socket.on("getMsg", (data) => {
+        console.log("Uma nova mensagem");
+        console.log(data);
+        socket.broadcast.to(data.toid).emit("sendMsg", {
+          msg: data.msg,
+          name: data.name,
+        });
+      });
+
+      socket.on("disconnect", () => {
+        console.log("Alguém foi desconectado");
+        this.users.forEach((elemento, indice) => {
+          if (elemento.id === socket.id) {
+            this.users.splice(indice, 1);
+          }
+        });
+        this.io.emit("exit", this.users);
+      });
+    });
+  }
+
+  routesConfig() {
+    this.appRoutes();
+    this.socketEvents();
+  }
+}
+module.exports = Routes;
